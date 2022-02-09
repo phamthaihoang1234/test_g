bde36a1b04baf5305b372f185a817e80b31b35a1
Hoang Pham <phamthaihoang1412@gmail.com>
2022-02-09 13:46:34 +0700

    test

--- src/java/context/DBContext.java
+++ src/java/context/DBContext.java
@@ -25,5 +25,14 @@ public class DBContext {
     private final String instance="";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
     private final String userID = "sa";
     private final String password = "sa";
+
+    /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
+    /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
+    public Connection getConnection()throws Exception {
+        String url = "jdbc:sqlserver://"+serverName+":"+portNumber + "\\" + instance +";databaseName="+dbName;
+        if(instance == null || instance.trim().isEmpty())
+            url = "jdbc:sqlserver://"+serverName+":"+portNumber +";databaseName="+dbName;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
 }
  
